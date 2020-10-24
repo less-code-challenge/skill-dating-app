41d57065a772c95171a8466a93a35c9d37e9361c
Marek Matczak <mmatczak@autograf.pl>
2020-10-24 14:17:56 +0200

    tiny refactoring

--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -1,19 +1,20 @@
-import { ModuleWithProviders, NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { SecurityService } from './security/security.service';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
-import { AuthHttpInterceptor } from './security/auth.http-interceptor';
-import { AuthGuard } from './security/auth.guard';
-import { RedirectToHomeGuard } from './security/redirect-to-home.guard';
-import { AfterSignInCallbackComponent } from './security/after-sign-in-callback.component';
-import { NavigationComponent } from './navigation/navigation.component';
-import { OverlayComponent } from './overlay/overlay.component';
+import {ModuleWithProviders, NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {SecurityService} from './security/security.service';
+import {HTTP_INTERCEPTORS} from '@angular/common/http';
+import {AuthHttpInterceptor} from './security/auth.http-interceptor';
+import {AuthGuard} from './security/auth.guard';
+import {RedirectToHomeGuard} from './security/redirect-to-home.guard';
+import {AfterSignInCallbackComponent} from './security/after-sign-in-callback.component';
+import {NavigationComponent} from './navigation/navigation.component';
+import {OverlayComponent} from './overlay/overlay.component';
 
-import { InputComponent } from './forms/input/input.component';
-import { AvatarComponent } from './avatar/avatar.component';
-import { AppHeaderComponent } from './app-header/app-header.component';
-import { SearchInputComponent } from './search/search-input/search-input.component';
+import {InputComponent} from './forms/input/input.component';
+import {AvatarComponent} from './avatar/avatar.component';
+import {AppHeaderComponent} from './app-header/app-header.component';
+import {SearchInputComponent} from './search/search-input/search-input.component';
 import {ReactiveFormsModule} from '@angular/forms';
+import {RouterModule} from '@angular/router';
 
 const exportedComponents = [
   OverlayComponent,
@@ -21,10 +22,17 @@ const exportedComponents = [
   InputComponent,
   AvatarComponent,
 ];
+
+const usedAndExportedModules = [
+  CommonModule,
+  ReactiveFormsModule,
+  RouterModule
+];
+
 @NgModule({
   declarations: [AfterSignInCallbackComponent, ...exportedComponents, AppHeaderComponent, SearchInputComponent],
-  imports: [CommonModule, ReactiveFormsModule],
-  exports: [...exportedComponents, AppHeaderComponent, SearchInputComponent],
+  imports: [...usedAndExportedModules],
+  exports: [...exportedComponents, ...usedAndExportedModules, AppHeaderComponent, SearchInputComponent],
 })
 export class SharedModule {
   static forRoot(): ModuleWithProviders<SharedModule> {
--- frontend/src/app/skill-dating/skill-dating.module.ts
+++ frontend/src/app/skill-dating/skill-dating.module.ts
@@ -1,26 +1,27 @@
 import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
 import {HomeDialogComponent} from './components/home-dialog/home-dialog.component';
 import {PopularSkillsComponent} from './components/home-dialog/popular-skills/popular-skills.component';
 import {SharedModule} from '../shared/shared.module';
-import {RouterModule} from '@angular/router';
 import {EditProfileDialogComponent} from './components/edit-profile-dialog/edit-profile-dialog.component';
 import {ProfileDialogComponent} from './components/profile-dialog/profile-dialog.component';
 import {LandingPageComponent} from './components/landing-page/landing-page.component';
-import { SearchAllDialogComponent } from './components/search-all-dialog/search-all-dialog.component';
-import { SearchSkillsDialogComponent } from './components/search-skills-dialog/search-skills-dialog.component';
-import { SearchProfilesDialogComponent } from './components/search-profiles-dialog/search-profiles-dialog.component';
+import {SearchAllDialogComponent} from './components/search-all-dialog/search-all-dialog.component';
+import {SearchSkillsDialogComponent} from './components/search-skills-dialog/search-skills-dialog.component';
+import {SearchProfilesDialogComponent} from './components/search-profiles-dialog/search-profiles-dialog.component';
 
 const dialogs = [
   LandingPageComponent,
   HomeDialogComponent,
   ProfileDialogComponent,
   EditProfileDialogComponent,
+  SearchAllDialogComponent,
+  SearchSkillsDialogComponent,
+  SearchProfilesDialogComponent
 ];
 
 @NgModule({
-  declarations: [...dialogs, PopularSkillsComponent, SearchAllDialogComponent, SearchSkillsDialogComponent, SearchProfilesDialogComponent],
-  imports: [CommonModule, RouterModule, SharedModule],
+  declarations: [...dialogs, PopularSkillsComponent],
+  imports: [SharedModule],
   exports: [...dialogs],
 })
 export class SkillDatingModule {
