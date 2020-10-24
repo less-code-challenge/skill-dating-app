30a8a8e61fbf4917a782ff672ebbb0772cff884b
Marek Matczak <mmatczak@autograf.pl>
2020-10-24 23:08:34 +0200

    skill items on result list implemented

--- frontend/src/app/skill-dating/components/result-list/skill-item/skill-item.component.ts
+++ frontend/src/app/skill-dating/components/result-list/skill-item/skill-item.component.ts
@@ -1,15 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'sd-skill-item',
   templateUrl: './skill-item.component.html',
   styleUrls: ['./skill-item.component.scss']
 })
-export class SkillItemComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+export class SkillItemComponent {
+  @Input()
+  value: string;
 }
