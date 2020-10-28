aaca28e67654009c938d81d4beac873dcd6465c4
Marek Matczak <mmatczak@autograf.pl>
2020-10-28 23:00:17 +0100

    caching skill popularity with 3 minutes refresh

--- frontend/src/app/skill-dating/services/skill.service.ts
+++ frontend/src/app/skill-dating/services/skill.service.ts
@@ -1,9 +1,12 @@
 import {Injectable} from '@angular/core';
-import {Observable} from 'rxjs';
+import {Observable, timer} from 'rxjs';
 import {HttpClient} from '@angular/common/http';
 import {environment} from '../../../environments/environment';
 import {PopularSkillsTo} from '../model/popular-skills.to';
 import {SkillTo} from '../model/skill.to';
+import {shareReplay, switchMap, take} from 'rxjs/operators';
+
+const refreshInterval = 3 * 60 * 1000; // 3 minutes
 
 @Injectable({
   providedIn: 'root',
@@ -11,11 +14,21 @@ import {SkillTo} from '../model/skill.to';
 export class SkillService {
   private readonly backendUri = environment.backendUri;
 
+  private popularSkillsCache$: Observable<PopularSkillsTo>;
+
   constructor(private readonly http: HttpClient) {
   }
 
   getPopularSkills(): Observable<PopularSkillsTo> {
-    return this.http.get<PopularSkillsTo>(`${this.backendUri}/skill-popularity`);
+    if (!this.popularSkillsCache$) {
+      // Set up timer that ticks every 3 minutes
+      const timer$ = timer(0, refreshInterval);
+      this.popularSkillsCache$ = timer$.pipe(
+        switchMap(() => this.http.get<PopularSkillsTo>(`${this.backendUri}/skill-popularity`)),
+        shareReplay(1)
+      );
+    }
+    return this.popularSkillsCache$.pipe(take(1));
   }
 
   findAll(): Observable<string[]> {
