41a45bc306dd00252c70358cb354af7e0b432709
Marek Matczak <mmatczak@autograf.pl>
2020-10-30 18:03:43 +0100

    Denying creating and updating other profiles

--- backend/core/src/adapter/rest/user-profile.rest.ts
+++ backend/core/src/adapter/rest/user-profile.rest.ts
@@ -1,6 +1,7 @@
 import {NextFunction, Request, Response} from 'express-serve-static-core';
 import userProfileAppService from '../../app-service/user-profile.app-service';
 import {Username} from '../../domain-model/user-profile';
+import {getSecurityContextFrom} from '../../security-context';
 
 export function getOne(req: Request, res: Response, next: NextFunction): void {
   const username = req.params?.['username'];
@@ -13,7 +14,18 @@ export function getOne(req: Request, res: Response, next: NextFunction): void {
 export function createNewOrUpdateExisting(req: Request, res: Response, next: NextFunction): void {
   const userProfileAttributes = req.body;
   const username = userProfileAttributes?.[Username.attributeName] || req.params?.[Username.attributeName];
-  // const securityContext = getSecurityContextFrom(req);
+
+  userProfileIsAboutToBeCreatedOrUpdatedByItsOwner(username, req)
+    .then(
+      () => userProfileAppService.createNewUserProfileOrUpdateExistingOne(username, userProfileAttributes).then(userProfile => res.status(201).send(userProfile.toPlainAttributes())),
+      () => res.sendStatus(401)
+    )
+    .catch(next); // handle globally
+
+  function userProfileIsAboutToBeCreatedOrUpdatedByItsOwner(username: string, req: Request): Promise<void> {
+    const securityContext = getSecurityContextFrom(req);
+    return securityContext.currentUsername === username ? Promise.resolve() : Promise.reject();
+  }
 
   userProfileAppService.createNewUserProfileOrUpdateExistingOne(username, userProfileAttributes)
     .then(userProfile => res.status(201).send(userProfile.toPlainAttributes()))
